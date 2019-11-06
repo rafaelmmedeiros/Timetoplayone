fa6e2a01e5c9650b12a9cafb60644a76cfd82564
rafaelmmedeiros <rafael.einherjar@gmail.com>
2019-11-05 23:39:27 -0300

    0.0.3 - Estrutura Inicial do App (Client)

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Header, Icon, List } from 'semantic-ui-react'
 import './App.css';
+import axios from 'axios';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    values: []
+  }
+
+  componentDidMount() { // Quando o componente monta coloca os dois valores dentro do array.
+    axios.get('http://localhost:5000/api/values')
+      .then((response) => {
+        this.setState({
+          values: response.data
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header as='h2'>
+          <Icon
+            name='calendar check outline'
+          />
+          <Header.Content>
+            Hora de Tocar
+              <Header.Subheader>
+              Gerenciamento de Estudos
+              </Header.Subheader>
+          </Header.Content>
+        </Header>
+
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.nome} {value.sobrenome} Idade: {value.idade}</List.Item>
+          ))}
+        </List>
+
+      </div>
+    );
+  }
 }
 
 export default App;
