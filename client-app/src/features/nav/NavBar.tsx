0529c760a954b416dd23cadddcbe52b131fee6ae
rafaelmmedeiros <rafael.einherjar@gmail.com>
2020-07-10 16:40:43 -0300

    0.0.1.49 - Navbar Responsiva

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -3,21 +3,28 @@ import { Menu, Container, Icon } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
 import { NavLink } from "react-router-dom";
 import { RootStoreContext } from "../../app/stores/rootStore";
+import { useMediaQuery } from "react-responsive";
 
 const NavBar: React.FC = () => {
   const rootStore = useContext(RootStoreContext);
   const { user, logout } = rootStore.userStore;
 
+  const isMobile = useMediaQuery({ query: "(max-width: 750px)" });
+
   return (
     <Menu fixed="top" inverted>
       <Container>
+        {/* ----DASHBOARD---- */}
         <Menu.Item header as={NavLink} exact to="/maindashboard">
           <Icon name="calendar check outline" size="big" />
-          Dashboard
+          {isMobile ? "Go!" : "Dashboard"}
         </Menu.Item>
-        {user && <Menu.Item icon="user" as={NavLink} to={`/profile/${user.username}`} />}
-        {user && <Menu.Item icon="road" as={NavLink} exact to="/takingtheroad" />}
-        {user && <Menu.Item position="right" icon="sign-out" onClick={logout} />}
+        {/* ----PROFILE---- */}
+        {user && <Menu.Item content={isMobile ? "" : "Profile"} icon="user" as={NavLink} to={`/profile/${user.username}`} />}
+        {/* ----TAKING THE ROAD---- */}
+        {user && <Menu.Item content={isMobile ? "" : "Taking The Road"} icon="road" as={NavLink} exact to="/grupos" />}
+        {/* ----LOGOUT---- */}
+        {user && <Menu.Item content={isMobile ? "" : "Logout"} position="right" icon="sign-out" onClick={logout} />}
       </Container>
     </Menu>
   );
