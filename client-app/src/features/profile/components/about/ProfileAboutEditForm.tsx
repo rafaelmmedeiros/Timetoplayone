68991e40db478c5ebbe9ee6b861ea08849aba394
rafaelmmedeiros <rafael.einherjar@gmail.com>
2020-07-10 20:44:34 -0300

    0.0.1.51 - About Validation

--- client-app/src/features/profile/components/about/ProfileAboutEditForm.tsx
+++ client-app/src/features/profile/components/about/ProfileAboutEditForm.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import { IProfile } from "../../../../app/models/profile";
 import { Form as FinalForm, Field } from "react-final-form";
 import { observer } from "mobx-react-lite";
-import { combineValidators, isRequired } from "revalidate";
+import { combineValidators, isRequired, composeValidators, hasLengthBetween, hasLengthLessThan } from "revalidate";
 import { Form, Button } from "semantic-ui-react";
 import TextInput from "../../../../app/common/form/TextInput";
 import TextAreaInput from "../../../../app/common/form/TextAreaInput";
 
 const validate = combineValidators({
-  displayName: isRequired("displayName"),
+  displayName: composeValidators(
+    isRequired({ message: "Is required" }),
+    hasLengthBetween(3, 15)({ message: "Must be beetween 3 and 15 characters" })
+  )(),
+  bio: composeValidators(
+    hasLengthLessThan(250)({ message: "Maximum of 250 characters" })
+  )(),
 });
 
 interface IProps {
@@ -25,7 +31,7 @@ const ProfileAboutEditForm: React.FC<IProps> = ({ updateProfile, profile }) => {
       render={({ handleSubmit, invalid, pristine, submitting }) => (
         <Form onSubmit={handleSubmit} error>
           <Field name="displayName" component={TextInput} placeholder="Display Name" value={profile!.displayName} />
-          <Field name="bio" component={TextAreaInput} rows={3} placeholder="Bio" value={profile!.bio} />
+          <Field name="bio" component={TextAreaInput} rows={5} placeholder="Bio" value={profile!.bio} />
           <Button loading={submitting} floated="right" disabled={invalid || pristine} positive content="Update profile" />
         </Form>
       )}
