1d6c9c043861095b921ad632ea86fb5e262a8330
rafaelmmedeiros <rafael.einherjar@gmail.com>
2020-07-11 19:52:14 -0300

    0.0.1.55 - Delete Repertoire (Server)

--- API/Controllers/RepertoiresController.cs
+++ API/Controllers/RepertoiresController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Application.AppTrainer.Repertoires;
 using MediatR;
@@ -7,18 +8,25 @@ namespace API.Controllers
 {
     public class RepertoiresController : BaseController
     {
-        // FUNC: Retorna repertorios de um usuario.
+        //  FUNC: Retorna repertorios de um usuario.
         [HttpGet("{username}")]
         public async Task<ActionResult<UserRepertoire>> Get(string username)
         {
             return await Mediator.Send(new Details.Query { Username = username });
         }
 
-        // FUNC: Criar novo Repertoire.
+        //  FUNC: Criar novo Repertoire.
         [HttpPost]
         public async Task<ActionResult<Unit>> Create(Create.Command command)
         {
             return await Mediator.Send(command);
+        }
+
+        //  FUNC: Deleta através do uuid (Guid for Microsoft) recebido.
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        {
+            return await Mediator.Send(new Delete.Command { Id = id });
         }
     }
 }
