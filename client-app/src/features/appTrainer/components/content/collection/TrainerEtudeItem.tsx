30f6350f62da45822d6263c64300c2e3410971ac
rafaelmmedeiros <rafael.einherjar@gmail.com>
2020-07-16 23:28:44 -0300

    0.0.1.73 - Date FNS working

--- client-app/src/features/appTrainer/components/content/collection/TrainerEtudeItem.tsx
+++ client-app/src/features/appTrainer/components/content/collection/TrainerEtudeItem.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import { observer } from "mobx-react-lite";
 import { IEtude } from "../../../../../app/models/appTrainer/userCollection";
 import { Card, Button, Icon } from "semantic-ui-react";
-import { format } from 'date-fns';
+import { format } from "date-fns";
 
 const TrainerEtudeItem: React.FC<{ etude: IEtude }> = ({ etude }) => {
+  var today = new Date();
+  var last = new Date(etude.lastPlayed);
+  var diff = today.valueOf() - last.valueOf();
+
   return (
     <Card key={etude.id} color="red">
       <Card.Content>
         <Card.Header>{etude.title}</Card.Header>
-        <Card.Meta>{etude.lastPlayed}</Card.Meta>
+        <Card.Meta>Last Played {format(diff, "d") + " days ago."}</Card.Meta>
       </Card.Content>
       <Card.Content extra>
         <Button basic color="violet" floated="right">
