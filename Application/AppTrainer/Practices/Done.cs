941d56fcf0ff81745fc29ec6f421a5e3e5670210
rafaelmmedeiros <rafael.einherjar@gmail.com>
2020-08-06 16:12:06 -0300

    0.0.2.11 - Chapter Creation Server

--- Application/AppTrainer/Practices/Done.cs
+++ Application/AppTrainer/Practices/Done.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Interfaces;
+using Domain.AppTrainer;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -31,19 +32,34 @@ namespace Application.AppTrainer.Practices
 
                 var etude = await _context.Etudes.FindAsync(request.Id);
 
+                var chapter = await _context.Chapters.FirstOrDefaultAsync(x => x.Day == DateTime.Today && x.AppUserId == user.Id);
+
                 //  SECURITY
+
+                //  EDIT CHAPTER
+                if (chapter == null)
+                {
+                    var chapterToCreate = new Chapter
+                    {
+                        Day = DateTime.Today,
+                        TotalTime = etude.Time,
+                        TotalEtudes = 1,
+                        AppUserId = user.Id
+                    };
+
+                    _context.Chapters.Add(chapterToCreate);
+                }
+                else
+                {
+                    chapter.TotalTime += etude.Time;
+                    chapter.TotalEtudes++;
+                }
 
                 //  EDIT ETUDE
                 etude.Played += etude.Time;
                 etude.Executions++;
                 etude.LastPlayed = DateTime.Now;
 
-
-
-
-                //  EDIT CHAPTER
-                // Se for NULL NEW CHAPTER
-                
                 var success = await _context.SaveChangesAsync() > 0;
 
                 if (success) return Unit.Value;
