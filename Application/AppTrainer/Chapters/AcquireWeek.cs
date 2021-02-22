66490d79ab9d5d8da5c0b4fd55deb2b16d954885
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-02-22 16:57:12 -0300

    Chapaters Week Server

--- Application/AppTrainer/Chapters/AcquireWeek.cs
+++ Application/AppTrainer/Chapters/AcquireWeek.cs
@@ -39,25 +39,59 @@ namespace Application.AppTrainer.Chapters
                     .AsQueryable();
 
                 var chapters = queryable.ToList();
-                var chaptersToProcessing = new List<ChapterWeekDto>();
+                var chaptersOnWeek = new List<ChapterWeekDto>();
+                var chaptersToComplete = new List<ChapterWeekDto>();
 
+                // ADD TO LIST CHAPATERS INCLUDE ON THE LAST SEVEN DAYS
                 foreach (var chapter in chapters)
                 {
                     var userChapter = new ChapterWeekDto
                     {
                         Id = chapter.Id,
-                        Day = chapter.Day
+                        Day = chapter.Day,
+                        TotalTime = chapter.TotalTime
                     };
 
-                    if ((DateTime.Now - userChapter.Day).TotalDays <= 7)
+                    if ((DateTime.Today - userChapter.Day).TotalDays <= 7)
                     {
-                        chaptersToProcessing.Add(userChapter);
+                        chaptersOnWeek.Add(userChapter);
                     }
+                }
+
+                // FIND THE BEST DAY
+                var bestWeekDay = chaptersOnWeek.Max(x => x.TotalTime);
+
+                // ADD ABSENT CHAPATER
+                for (int interval = 0; interval < 7; interval++)
+                {
+                    var element = new ChapterWeekDto { };
+
+                    foreach (var chapter in chaptersOnWeek)
+                    {
+                        if ((DateTime.Today - chapter.Day).TotalDays == interval)
+                        {
+                            chapter.DifferenceToBestDay = Math.Round((double)chapter.TotalTime / bestWeekDay, 2);
+
+                            element = chapter;
+                            break;
+                        }
+                        else
+                        {
+                            var notPlayedElement = new ChapterWeekDto
+                            {
+                                Day = DateTime.Today.AddDays(-interval),
+                            };
+
+                            element = notPlayedElement;
+                        }
+                    }
+
+                    chaptersToComplete.Add(element);
                 }
 
                 return new ChaptersWeekEnvelope
                 {
-                    WeekChapters = chaptersToProcessing
+                    WeekChapters = chaptersToComplete
                 };
             }
         }
--- Application/AppTrainer/Chapters/Others/ChapterWeekDto.cs
+++ Application/AppTrainer/Chapters/Others/ChapterWeekDto.cs
@@ -7,6 +7,13 @@ namespace Application.AppTrainer.Chapters.Others
         public Guid Id { get; set; }
         public DateTime Day { get; set; }
         public int TotalTime { get; set; }
+        public double DifferenceToBestDay { get; set; }
+
+
+
+
+
+
         public int TotalEtudes { get; set; }
         public int Objective { get; set; }
     }
