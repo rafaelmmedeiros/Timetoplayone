742243f6db1efc71e5147da4d2cb073e96621122
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-02-23 16:38:12 -0300

    Week Chapters Ok

--- client-app/src/features/appTrainer/components/content/chapters/TrainerChapterWeekDaysItem.tsx
+++ client-app/src/features/appTrainer/components/content/chapters/TrainerChapterWeekDaysItem.tsx
@@ -1,13 +1,18 @@
+import { format } from "date-fns";
 import { observer } from "mobx-react-lite";
 import React from "react";
 import { Grid, Header, Progress } from "semantic-ui-react";
 import { IChapter } from "../../../../../app/models/appTrainer/domain/chapter";
 
 const TrainerChapterWeekDaysItem: React.FC<{ chapter: IChapter }> = ({ chapter }) => {
+  let hours = (chapter.totalTime / 60).toFixed(1);
+
   return (
     <Grid.Row>
-      <Header>{chapter.day}</Header>
-      <Progress size="large" percent={chapter.differenceToBestDay} progress indicating>SOMETHING GOEs HERE</Progress>
+      <Header>{format(chapter.day, "eee do MMM")}</Header>
+      <Progress size="large" percent={chapter.differenceToBestDay} progress indicating>
+        {hours} /H
+      </Progress>
     </Grid.Row>
   );
 };
