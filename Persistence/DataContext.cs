95551cd0a10b6ae706af5c3ca0a60d6d45bc0c9a
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-02-25 14:39:02 -0300

    Reformat

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -4,12 +4,9 @@ using Domain.AppTrainer;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Persistence
-{
-    public class DataContext : IdentityDbContext<AppUser>
-    {
-        public DataContext(DbContextOptions options) : base(options)
-        {
+namespace Persistence {
+    public class DataContext : IdentityDbContext<AppUser> {
+        public DataContext(DbContextOptions options) : base(options) {
         }
 
         // Entidades a serem geradas pelo Entity Framework
